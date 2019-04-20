999f6532234eaf85af5403d9709aba17eff691b5
MichalAW <michal.wanta@gmail.com>
2019-04-20 09:13:09 +0200

    please accept this

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
 var url = 'http://restcountries.eu/rest/v1/name/';
 var countriesList = document.getElementById('countries');
+var prefix = "https://cors-anywhere.herokuapp.com/";
 
 function searchCountries() {
     var countryName = document.getElementById('country-name').value;
 
     if (!countryName.length) countryName = 'Poland';
 
-    fetch(url + countryName)
+    fetch(prefix + url + countryName)
         .then(function(resp) {
             return resp.json();
         })
-        .then(showCountriesList);
+    .then(showCountriesList);
 }
 
 function showCountriesList(resp) {
+    if (!Array.isArray(resp)) {
+        return alert('I\'m sorry, please try again');
+    }
     countriesList.innerHTML = '';
     resp.forEach(function(item) {
         var li = document.createElement('li');
